5c0990d9122d12a8b36abc772879fd19ffcaa8cc
sujit995 <sujitmishra402@gmail.com>
2021-11-17 09:34:47 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:contact@sujitmishra115@gmail.com">Contact: sujitmishra115@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialContainer>
+        <SocialIcons href="https://github.com">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://instagram.com">
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+      </SocialContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I am <br />
+        Sujit Mishra
+        <h6>Learner & Developer</h6>
+      </SectionTitle>
+      <SectionText>
+      I love learning about new technologies, and solving problems to build better and scalable products.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
